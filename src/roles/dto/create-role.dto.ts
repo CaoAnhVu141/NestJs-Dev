f3cc43d62b3c66af68ab293be5ed815cb5b90ed7
caoanhvu <caoanhvu@citgroup.vn>
2025-06-14 23:54:57 +0700

    update commit code

---
+++ src/roles/dto/create-role.dto.ts
@@ -0,0 +1,19 @@
+import { IsArray, IsMongoId, IsNotEmpty } from "class-validator";
+import mongoose from "mongoose";
+
+export class CreateRoleDto {
+
+    @IsNotEmpty({ message: 'Name không được để trống', })
+    email: string;
+
+    @IsNotEmpty({ message: 'Description không được để trống', })
+    description: string;
+
+    @IsNotEmpty({ message: 'IsActive không được để trống', })
+    isActive: boolean;
+
+    @IsNotEmpty({ message: 'Permisstions không được để trống', })
+    @IsArray()
+    @IsMongoId({each: true})
+    permissions: mongoose.Types.ObjectId[]
+}
